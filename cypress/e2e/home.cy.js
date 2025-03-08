44d8c2b7d86fa2fd87d38d9e26b8172d7e472364
ishaq <kmishaq08@gmail.com>
2025-03-08 23:13:40 +0500

    add fixtures

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,27 +1,33 @@
 describe("landing page ", () => {
   //Test setup, or hooks
-  before(() => {
+  before(function () {
     //before do any thing run this block
     // run 1 time
+    cy.fixture("user.json").as("myUser")
      cy.visit('https://www.saucedemo.com/v1/');
-     cy.get('#login-button').should('be.visible');
+    cy.get('#login-button').should('be.visible');
+
+    
   })
-  beforeEach(() => {
+  beforeEach(function () {
     //run before every test
+  })
+  it.only("perform login" ,function () {
     cy.visit('https://www.saucedemo.com/v1/');
     cy.get('#login-button').should("be.visible");
-    cy.get('[data-test="username"]').type("standard_user", { slow: 0.5 });
-    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.get('[data-test="username"]').type(this.myUser.validUser.userName);
+    cy.get('[data-test="password"]').type(this.myUser.validUser.password);
     cy.get('#login-button').click();
     cy.url().should("include", "inventory.html")
+    
   })
   it('Add to cart', () => {
-      cy.get(cy.get(':nth-child(1) > .pricebar > .btn_primary').click())
-      cy.get(cy.get('.fa-layers-counter').should("contain", 1));
+    cy.get(':nth-child(1) > .pricebar > .btn_primary').click();
+      cy.get('.fa-layers-counter').should('contain', 1);
   });
-  it ("perform logout", () => {
+  it.only("perform logout", () => {
     cy.get('.bm-burger-button > button').click();
-    cy.pause();
+   // cy.pause();
     cy.get('#logout_sidebar_link').click();
    cy.get('#login-button').should('be.visible');
   })
@@ -37,13 +43,22 @@ describe("landing page ", () => {
   });
 })
 
-//headed test :- saw browser , slow
+//headed test :- saw browser
+      // npx cypress open
+      // slower the headless
 //headless test :- not show browser , false tests are show in the report , run in terminal or server
-  // headless mode run at the end of project
-  // fast
-// npx cypress run
+      // headless mode run at the end of project
+      // fast
+      // npx cypress run
 
 //cy.wait(5000); // wait for 5 sec , its recommended to use
 //cy.reload()   // reload the page
 //cy.pause(); // pause the test . when you allow it then move to next step
 
+
+//fixtures
+// in fixtures folder we write test data
+// test data is data which is used in testing
+// data driven testing: data comes from other soures
+// when we write some data in fixture , and now we need to add or change data , so we samply change in the fixtures , and that is also data driven testing
+// where we use fixtures , the functoin is normal function
