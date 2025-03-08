d9dae8665e03f097359fa9ea4a240cd3c89a3446
ishaq <kmishaq08@gmail.com>
2025-03-08 11:05:46 +0500

    write test cases and hooks

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,14 +1,41 @@
 describe("landing page ", () => {
-  it("verify if landing page is reacheable", () => {
-    cy.visit('https://www.saucedemo.com/v1/');
-    cy.get('#login-button').should("be.visible");
+  //Test setup, or hooks
+  before(() => {
+    //before do any thing run this block
+    // run 1 time
+     cy.visit('https://www.saucedemo.com/v1/');
+     cy.get('#login-button').should('be.visible');
   })
-  it("Login with valid credentials", () => {
+  beforeEach(() => {
+    //run before every test
     cy.visit('https://www.saucedemo.com/v1/');
     cy.get('#login-button').should("be.visible");
     cy.get('[data-test="username"]').type("standard_user", { slow: 0.5 });
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('#login-button').click();
     cy.url().should("include", "inventory.html")
- })
+  })
+  it("verify if landing page is reacheable", () => { })
+
+  it("Login with valid credentials", () => { })
+
+  it('Add to cart', () => {
+      cy.get(cy.get(':nth-child(1) > .pricebar > .btn_primary').click())
+      cy.get(cy.get('.fa-layers-counter').should("contain", 1));
+  });
+  it("perform logout", () => {
+  cy.get('.bm-burger-button > button').click();
+    cy.get('#logout_sidebar_link').click();
+   cy.get('#login-button').should('be.visible');
+  })
+  
+  it 
+  // Test Tear Down
+  after(() => { 
+    //run at the end of test cases 
+    // run 1 time
+  });
+  afterEach(() => {
+    // run at the every end of test
+  });
 })
