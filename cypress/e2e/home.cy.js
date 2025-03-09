8a798ed1016bd6410aac2bd981d33693a4567aba
ishaq <kmishaq08@gmail.com>
2025-03-09 15:51:59 +0500

    fixed fixtures error

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,64 +1,47 @@
-describe("landing page ", () => {
-  //Test setup, or hooks
+describe('Landing Page', () => {
+  // Run once before all tests
   before(function () {
-    //before do any thing run this block
-    // run 1 time
-    cy.fixture("user.json").as("myUser")
-     cy.visit('https://www.saucedemo.com/v1/');
-    cy.get('#login-button').should('be.visible');
-
-    
-  })
-  beforeEach(function () {
-    //run before every test
-  })
-  it.only("perform login" ,function () {
+    cy.fixture('user.json').as('myUser');
     cy.visit('https://www.saucedemo.com/v1/');
-    cy.get('#login-button').should("be.visible");
-    cy.get('[data-test="username"]').type(this.myUser.validUser.userName);
-    cy.get('[data-test="password"]').type(this.myUser.validUser.password);
-    cy.get('#login-button').click();
-    cy.url().should("include", "inventory.html")
-    
-  })
-  it('Add to cart', () => {
+    cy.get('#login-button').should('be.visible');
+  });
+
+  // Run before each test case
+  beforeEach(function () {
+    cy.visit('https://www.saucedemo.com/v1/');
+
+    // Ensure fixture data is loaded before login
+    cy.get('myUser').then((user) => {
+      cy.get('[data-test="username"]').type(user.validUser.userName);
+      cy.get('[data-test="password"]').type(user.validUser.password);
+      cy.get('#login-button').click();
+      cy.url().should('include', 'inventory.html');
+    });
+  });
+
+  it('Perform Login', function () {
+    cy.url().should('include', 'inventory.html');
+  });
+
+  it('Add to cart', function () {
     cy.get(':nth-child(1) > .pricebar > .btn_primary').click();
-      cy.get('.fa-layers-counter').should('contain', 1);
+    cy.get('.fa-layers-counter').should('contain', 1);
   });
-  it.only("perform logout", () => {
+
+  it('Perform Logout', function () {
     cy.get('.bm-burger-button > button').click();
-   // cy.pause();
+    cy.pause();
     cy.get('#logout_sidebar_link').click();
-   cy.get('#login-button').should('be.visible');
-  })
-  
-  it 
-  // Test Tear Down
-  after(() => { 
-    //run at the end of test cases 
-    // run 1 time
+    cy.get('#login-button').should('be.visible');
   });
+
+  // Run once after all test cases
+  after(() => {
+    cy.log('All tests completed!');
+  });
+
+  // Run after each test case
   afterEach(() => {
-    // run at the every end of test
+    cy.log('Test case completed!');
   });
-})
-
-//headed test :- saw browser
-      // npx cypress open
-      // slower the headless
-//headless test :- not show browser , false tests are show in the report , run in terminal or server
-      // headless mode run at the end of project
-      // fast
-      // npx cypress run
-
-//cy.wait(5000); // wait for 5 sec , its recommended to use
-//cy.reload()   // reload the page
-//cy.pause(); // pause the test . when you allow it then move to next step
-
-
-//fixtures
-// in fixtures folder we write test data
-// test data is data which is used in testing
-// data driven testing: data comes from other soures
-// when we write some data in fixture , and now we need to add or change data , so we samply change in the fixtures , and that is also data driven testing
-// where we use fixtures , the functoin is normal function
+});
